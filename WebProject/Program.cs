8845fc3b401524cf68a5592ffd9d4b3a044c72b1
Evgen <Evgen@DESKTOP-TFNLO9V>
2023-09-26 20:43:12 +0300

    Добавил контроллер

--- WebProject/Program.cs
+++ WebProject/Program.cs
@@ -1,10 +1,23 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddControllersWithViews();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 //Загрузка конфигурацииъ
 
-var configuration = app.Configuration;
-var greetings = configuration["CustomGreetings"];
-app.MapGet("/", () => greetings);
+//app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+app.MapGet("/throw", () => {
+    throw new ApplicationException("Ошибка!");
+});
+
+app.MapDefaultControllerRoute();
+
 
 app.Run();
