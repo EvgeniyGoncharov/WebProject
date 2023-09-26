9ac12099cfc308d2e3afceeb9e6b57fc249fb48e
Evgen <Evgen@DESKTOP-TFNLO9V>
2023-09-26 20:18:47 +0300

    Добавьте файлы проекта.

---
+++ WebProject/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//Загрузка конфигурацииъ
+
+var configuration = app.Configuration;
+var greetings = configuration["CustomGreetings"];
+app.MapGet("/", () => greetings);
+
+app.Run();
